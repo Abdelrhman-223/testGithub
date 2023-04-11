cbebac286f4ec66ff8af174a3238e9a840ed6599
abdo <abdelrhman@DESKTOP-G0SHSIH.(none)>
2023-04-11 12:25:35 +0200

    Add project files.

---
+++ WebApplication2/main.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="main.aspx.cs" Inherits="WebApplication2.main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" submitdisabledcontrols="False">
+        <asp:Calendar ID="C1" runat="server" OnSelectionChanged="Calendar1"></asp:Calendar>
+        <asp:Button runat="server" Text="Hide Calendar Title" OnClick="Unnamed1_Click"></asp:Button>
+        <asp:Button runat="server" OnClick="Unnamed5_Click" Text="Show Calendar Title"></asp:Button>
+        <br/>
+        <asp:Button runat="server" Text="Print Date" OnClick="Unnamed3_Click"></asp:Button>
+        <asp:Label runat="server"  Text="Print date here" ID="printDate"></asp:Label>
+        <br/>
+        <asp:Button runat="server" Text="Print Date Time" OnClick="Unnamed4_Click"></asp:Button>
+        <asp:Label runat="server"  Text="Print date and time here" ID="printDateTime"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/main.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Calendar1(object sender, EventArgs e)
+        {
+            printDate.Text = C1.SelectedDate.Day.ToString();
+
+        }
+
+        protected void Unnamed5_Click(object sender, EventArgs e)
+        {
+            C1.ShowTitle = true;
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            C1.ShowTitle = false;
+        }
+
+        protected void Unnamed3_Click(object sender, EventArgs e)
+        {
+            printDate.Text = C1.SelectedDate.ToShortDateString();
+        }
+
+        protected void Unnamed4_Click(object sender, EventArgs e)
+        {
+            printDateTime.Text = C1.SelectedDate.ToString();
+        }
+    }
+}
